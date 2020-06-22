a1ec092e677e79cf8552628125f8c54aec9e67e5
Khuong Dang <khuong.dang@morphotech.co.uk>
2020-06-22 17:18:17 +0700

    Fix error table change show item
    Fix error table change show item

--- src/components/atoms/dtc-table/dtc-table.comp.js
+++ src/components/atoms/dtc-table/dtc-table.comp.js
@@ -23,19 +23,26 @@ export const DTCTable = React.memo(
     const [sortedInfo, setSortedInfo] = useState({});
     const [searchText, setSearchText] = useState("");
     const [itemsPerPage, setItemsPerPage] = useState(5);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const filtered = (() => {
       if (searchText === "") {
         return dataSource;
       }
-      return dataSource.filter((bid) => {
+      const result = dataSource.filter((bid) => {
         return Object.entries(bid).find(([, value]) => {
           return (
             value && value.toString().toLowerCase().trim().includes(searchText.toLowerCase().trim())
           );
         });
       });
+      return result;
     })();
+
+    const handlePageSizeChange = (e) => {
+      setItemsPerPage(Number(e.key));
+      setCurrentPage(1);
+    };
 
     return (
       <div className={classNames(className)}>
@@ -46,7 +53,7 @@ export const DTCTable = React.memo(
             <Dropdown
               className="mx-2"
               overlay={
-                <Menu onClick={(e) => setItemsPerPage(Number(e.key))}>
+                <Menu onClick={handlePageSizeChange}>
                   <Menu.Item key={5}>5</Menu.Item>
                   <Menu.Item key={10}>10</Menu.Item>
                   <Menu.Item key={25}>25</Menu.Item>
@@ -77,13 +84,18 @@ export const DTCTable = React.memo(
         </div>
         <div className={`air__utils__scrollTable ${styles["custom-pagination"]}`}>
           <Table
+            showSorterTooltip={false}
             rowSelection={rowSelection}
             loading={{ indicator: <LoadingIndicator />, spinning: loading }}
             pagination={{
               pageSize: itemsPerPage,
               total: filtered.length,
               showTotal: (total, range) => `${range[0]} to ${range[1]} of ${total} entries`,
-              showLessItems: true
+              showLessItems: true,
+              onChange: (page, pageSize) => {
+                setCurrentPage(page);
+              },
+              current: currentPage
             }}
             scroll={{ x: true }}
             rowKey={(record) => record.id}
