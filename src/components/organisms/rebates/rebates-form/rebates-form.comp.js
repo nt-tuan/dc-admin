599a26293d4d833197ebcca7fb64abd2f898943d
thuy_vu <thuy.vu@morphotech.co.uk>
2020-07-30 14:51:20 +0700

    handle error rebates

--- src/components/organisms/rebates/rebates-form/rebates-form.comp.js
+++ src/components/organisms/rebates/rebates-form/rebates-form.comp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, forwardRef } from "react";
 import { Form, Select, Input, Button } from "antd";
 import { RegexConst, RouteConst } from "commons/consts";
 import { FormError, LoadingIndicator } from "components/atoms";
@@ -9,100 +9,106 @@ const { Option } = Select;
 
 const { FIELDS, LABELS } = REBATES_SCHEMA;
 
-export const RebatesForm = ({
-  title,
-  isEdit = false,
-  data = null,
-  companies = [],
-  productBrand = [],
-  onSubmit,
-  loading,
-  isProcessing
-}) => {
-  const [form] = Form.useForm();
+export const RebatesForm = forwardRef(
+  (
+    {
+      title,
+      isEdit = false,
+      data = null,
+      companies = [],
+      productBrand = [],
+      onSubmit,
+      loading,
+      isProcessing
+    },
+    ref
+  ) => {
+    const [form] = Form.useForm();
 
-  useEffect(() => {
-    data && form.setFieldsValue(data);
-  }, [form, data]);
+    useEffect(() => {
+      data && form.setFieldsValue(data);
+    }, [form, data]);
 
-  const handleFinish = (values) => {
-    onSubmit(values);
-  };
+    const handleFinish = (values) => {
+      onSubmit(values);
+    };
 
-  const renderFormItem = (field) => {
-    switch (field) {
-      case FIELDS.buyerCompanyName:
-        return (
-          <Select mode="multiple" disabled={isEdit}>
-            {companies.map((company, index) => (
-              <Option value={company.id} key={`${company}-${index}`}>
-                {company.name}
-              </Option>
-            ))}
-          </Select>
-        );
-      case FIELDS.productBrand:
-        return (
-          <Select disabled={isEdit}>
-            {productBrand.map((brand, index) => (
-              <Option value={brand} key={`${brand}-${index}`}>
-                {brand}
-              </Option>
-            ))}
-          </Select>
-        );
-      case FIELDS.rebatePercentage:
-        return <Input />;
-      default:
-        return null;
-    }
-  };
+    const renderFormItem = (field) => {
+      switch (field) {
+        case FIELDS.buyerCompanyName:
+          return (
+            <Select mode="multiple" disabled={isEdit}>
+              {companies.map((company, index) => (
+                <Option value={company.id} key={`${company}-${index}`}>
+                  {company.name}
+                </Option>
+              ))}
+            </Select>
+          );
+        case FIELDS.productBrand:
+          return (
+            <Select disabled={isEdit}>
+              {productBrand.map((brand, index) => (
+                <Option value={brand} key={`${brand}-${index}`}>
+                  {brand}
+                </Option>
+              ))}
+            </Select>
+          );
+        case FIELDS.rebatePercentage:
+          return <Input />;
+        default:
+          return null;
+      }
+    };
 
-  return (
-    <Fragment>
-      {loading ? (
-        <div className="d-flex justify-content-center">
-          <LoadingIndicator />
-        </div>
-      ) : (
-        <div>
-          <h5>{title}</h5>
-          <div>Explanation text</div>
-          <Form
-            name="CreateRebatesForm"
-            form={form}
-            colon={false}
-            hideRequiredMark={true}
-            scrollToFirstError={true}
-            labelAlign="left"
-            className="mt-4"
-            onFinish={handleFinish}
-          >
-            {schema.map(({ name, label, rules }) => (
-              <Form.Item
-                name={name}
-                label={label}
-                key={name}
-                rules={rules}
-                labelCol={{ sm: { span: 8 }, md: { span: 8 }, lg: { span: 6 } }}
-              >
-                {renderFormItem(name)}
-              </Form.Item>
-            ))}
-            <div className="d-flex justify-content-center mt-4">
-              <Link to={RouteConst.REBATES}>
-                <Button type="primary">Cancel</Button>
-              </Link>
-              <Button type="primary" htmlType="submit" className="ml-2" disabled={isProcessing}>
-                {isEdit ? "Edit" : "Create"}
-              </Button>
-            </div>
-          </Form>
-        </div>
-      )}
-    </Fragment>
-  );
-};
+    return (
+      <Fragment>
+        {loading ? (
+          <div className="d-flex justify-content-center">
+            <LoadingIndicator />
+          </div>
+        ) : (
+          <div>
+            <h5>{title}</h5>
+            <div>Explanation text</div>
+            <Form
+              ref={ref}
+              name="CreateRebatesForm"
+              form={form}
+              colon={false}
+              hideRequiredMark={true}
+              scrollToFirstError={true}
+              labelAlign="left"
+              className="mt-4"
+              onFinish={handleFinish}
+            >
+              {schema.map(({ name, label, rules }) => (
+                <Form.Item
+                  name={name}
+                  label={label}
+                  key={name}
+                  rules={rules}
+                  labelCol={{ sm: { span: 8 }, md: { span: 8 }, lg: { span: 6 } }}
+                >
+                  {renderFormItem(name)}
+                </Form.Item>
+              ))}
+              <div className="d-flex justify-content-center mt-4">
+                <Link to={RouteConst.REBATES}>
+                  <Button type="primary">Cancel</Button>
+                </Link>
+                <Button type="primary" htmlType="submit" className="ml-2" disabled={isProcessing}>
+                  {isEdit ? "Edit" : "Create"}
+                </Button>
+              </div>
+            </Form>
+          </div>
+        )}
+      </Fragment>
+    );
+  }
+);
 
 const schema = [
   {
--- src/components/pages/create-rebates/create-rebates.page.js
+++ src/components/pages/create-rebates/create-rebates.page.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { RebatesForm } from "components/organisms";
 import { asyncErrorHandlerWrapper } from "utils/error-handler.util";
 import { RebatesService } from "services";
 import { RouteConst } from "commons/consts";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { REBATES_SCHEMA } from "commons/schemas";
+import { FormError } from "components/atoms";
 
 const { FIELDS } = REBATES_SCHEMA;
 
@@ -14,6 +15,7 @@ const CreateRebatesPage = () => {
   const [loading, setLoading] = useState(true);
   const [isCreating, setIsCreating] = useState(false);
   const history = useHistory();
+  const rebatesFormRef = useRef();
 
   useEffect(() => {
     asyncErrorHandlerWrapper(async () => {
@@ -30,14 +32,35 @@ const CreateRebatesPage = () => {
   const handleCreate = (values) => {
     setIsCreating(true);
     asyncErrorHandlerWrapper(async () => {
-      const data = {
-        brand: values[FIELDS.productBrand],
-        categoryName: "Perfume",
-        toCompanyIdList: values[FIELDS.buyerCompanyName],
-        value: values[FIELDS.rebatePercentage]
-      };
-      await RebatesService.createRebates(data);
-      history.push(RouteConst.REBATES);
+      try {
+        const data = {
+          brand: values[FIELDS.productBrand],
+          categoryName: "Perfume",
+          toCompanyIdList: values[FIELDS.buyerCompanyName],
+          value: values[FIELDS.rebatePercentage]
+        };
+        await RebatesService.createRebates(data);
+        history.push(RouteConst.REBATES);
+      } catch (error) {
+        const err = error.errors;
+        if (err.length) {
+          const companies = err[0][1];
+          const brand = err[1][1];
+          const msg = `Error in creating the Rebate. There is an existing rebate set for ${companies.join(
+            ", "
+          )} and ${brand}`;
+          rebatesFormRef.current.setFields([
+            {
+              name: "toCompanyName",
+              value: rebatesFormRef.current.getFieldValue("toCompanyName"),
+              errors: [<FormError msg={msg} />]
+            }
+          ]);
+          setIsCreating(false);
+        } else {
+          throw error;
+        }
+      }
     });
   };
 
@@ -50,6 +73,7 @@ const CreateRebatesPage = () => {
         onSubmit={handleCreate}
         loading={loading}
         isProcessing={isCreating}
+        ref={rebatesFormRef}
       />
     </div>
   );
