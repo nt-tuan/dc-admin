cd37b1301249c4317eab28f3c920ed7b604e677d
honglam1292 <honglam.1292@gmail.com>
2020-11-20 11:28:54 +0700

    Revert "DISTICHAIN-1177 change logo of login page"
    This reverts commit 856e558c782ad751fda8ad681f560e9161e2a2fb.

--- src/layouts/public/public.layout.js
+++ src/layouts/public/public.layout.js
@@ -1,28 +1,23 @@
 import React from "react";
 import styles from "./public-layout.module.scss";
-import footer from "assets/images/login/login-right.png";
 
 export const PublicLayout = React.memo(({ children }) => {
   return (
     <section id={styles["public-layout"]}>
-      <div className={`row no-gutters vh-100 ${styles.rightBlock}`}>
-        <div className="col-md-12 col-lg-5">
+      <div className="row no-gutters vh-100">
+        <div className="col-md-12 col-lg-6">
           <div id={styles.left} />
         </div>
-        <div id={styles.right} className="col-md-12 col-lg-7 justify-content-center">
-          <div className="container" style={{ paddingLeft: "10%" }}>
+        <div id={styles.right} className="col-md-12 col-lg-6 justify-content-center">
+          <div className="container">
             <img
               id={styles.logo}
               className="ml-2 mb-3 mt-md-5 mt-1"
               src={`${process.env.PUBLIC_URL}/images/logo.png`}
-              alt="logo"
+              alt={`${process.env.REACT_APP_COMPANY_NAME} logo`}
             />
             {children}
           </div>
-          <a href="https://distichain.com/" className={styles.link}>
-            Powered by Distichain Limited
-          </a>
-          <img className={styles.footer} src={footer} alt="logo" width></img>
         </div>
       </div>
     </section>
