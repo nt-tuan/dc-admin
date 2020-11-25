502ffffb874a09699d1a5b02798bb6eb968c53ec
honglam1292 <honglam.1292@gmail.com>
2020-11-25 15:47:59 +0700

    DISTICHAIN-1175 add array field variant

--- src/components/templates/product-mutation-template/components/VariantsDetails/index.js
+++ src/components/templates/product-mutation-template/components/VariantsDetails/index.js
@@ -1,13 +1,46 @@
 import React, { forwardRef, useState } from "react";
+import { Form, Input, Button, Row, Col } from "antd";
 import Field from "../CustomField/Field";
 
-const VariantDetails = forwardRef(({}, ref) => {
+const VariantDetails = ({ form }) => {
   const [fields, setFields] = useState([]);
   return (
-    <div>
-      <Field />
-    </div>
+    <Form form={form}>
+      <Form.List name="new-fields-item">
+        {(fields, { add, remove }) => (
+          <>
+            {fields.map((field, index) => (
+              <Form.Item
+                noStyle
+                shouldUpdate={(prevValues, curValues) => prevValues.name !== curValues.name}
+              >
+                {() => (
+                  <Form.Item
+                    {...field}
+                    name={[field.name, "name"]}
+                    fieldKey={[field.fieldKey, "name"]}
+                    rules={[{ required: true, message: "Missing Name" }]}
+                  >
+                    <Field />
+                  </Form.Item>
+                )}
+              </Form.Item>
+            ))}
+            <Form.Item className="mt-3">
+              <Button
+                type="primary"
+                onClick={() => add()}
+                style={{ minWidth: "100px" }}
+                // icon={<PlusOutlined />}
+              >
+                New Field
+              </Button>
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+    </Form>
   );
-});
+};
 
 export default VariantDetails;
