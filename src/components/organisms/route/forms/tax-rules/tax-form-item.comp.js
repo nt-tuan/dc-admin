66bcb4688314b1e207d223c916d53426accec5c4
Hnam <nam.lam@morphotech.co.uk>
2021-03-16 09:29:46 +0700

    mix file

---
+++ src/components/organisms/route/forms/tax-rules/tax-form-item.comp.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { Form, Select, Input, Radio, Button } from "antd";
+import { MinusOutlined } from "@ant-design/icons";
+import { FIELDS } from "./tax.chemas";
+// import { FIELDS } from "./tax.chemas";
+
+const { Option } = Select;
+
+function TaxFormItem({ handleFieldChange, dataForm, onRemoveTax }) {
+  const renderTaxRules = () => {
+    // console.log("dataForm",dataForm)
+    return Object.entries(dataForm).map(([key, valueData]) => {
+      if (valueData && valueData?.length) {
+        return valueData.map((val, idx) => {
+          let filteredFields = val?.data;
+          if (val?.dataFilter) {
+            filteredFields = val?.data?.filter((field) => !val?.dataFilter.includes(field?.name));
+          }
+          return (
+            <>
+              {key === "taxOther" && (
+                <div className="col-12 text-right ">
+                  <Button
+                    disabled={dataForm?.taxOther[0]?.data?.length === 1}
+                    // className="position-absolute"
+                    // style={{top:'100%',right:'15px'}}
+                    onClick={() => onRemoveTax(idx)}
+                    type="primary"
+                    danger
+                    size="small"
+                    shape="circle"
+                    icon={<MinusOutlined />}
+                  />
+                </div>
+              )}
+              {filteredFields &&
+                filteredFields.map(
+                  (
+                    {
+                      label,
+                      name,
+                      type,
+                      placeholder,
+                      rules,
+                      initValue,
+                      data,
+                      classNames,
+                      disabled,
+                      or
+                    },
+                    index
+                  ) => {
+                    const nameForm = `${key}-${name}-${idx}`;
+
+                    switch (type) {
+                      case "radio":
+                        return (
+                          <div key={`${nameForm} ${index}`} className="col-md-12">
+                            <Form.Item
+                              name={nameForm}
+                              label={label}
+                              className={`${classNames ? "col-12" : ""}`}
+                            >
+                              <Radio.Group
+                                defaultValue={initValue}
+                                className={`${
+                                  classNames ? "row justify-content-between" : "d-inline"
+                                }`}
+                                onChange={handleFieldChange(nameForm)}
+                              >
+                                {data &&
+                                  data.map((radio, key) => (
+                                    <Radio
+                                      key={`name-${key}`}
+                                      value={radio.value}
+                                      style={{ width: `${classNames ? "" : "auto"}` }}
+                                    >
+                                      {radio.name}
+                                    </Radio>
+                                  ))}
+                              </Radio.Group>
+                            </Form.Item>
+                          </div>
+                        );
+                      case "select":
+                        const formSelectType = (
+                          <Form.Item
+                            key={`${nameForm} ${index}`}
+                            label={label}
+                            className="col-6"
+                            name={nameForm}
+                            rules={[...rules]}
+                            labelCol={{ span: 24 }}
+                          >
+                            <Select
+                              placeholder={placeholder}
+                              onChange={handleFieldChange(nameForm)}
+                            >
+                              {initValue.map((item) => (
+                                <Option key={item.label} value={item.value}>
+                                  {item.label}
+                                </Option>
+                              ))}
+                            </Select>
+                          </Form.Item>
+                        );
+                        return (
+                          <>
+                            {dataForm[key][idx].dataFilter.length !== 0 ? (
+                              <div className="w-100" key={`${nameForm} ${index}-1`}>
+                                {formSelectType}
+                              </div>
+                            ) : (
+                              formSelectType
+                            )}
+                          </>
+                        );
+
+                      default:
+                        return (
+                          <>
+                            <Form.Item
+                              key={`${nameForm} ${index}`}
+                              label={label}
+                              className={`${or && or !== "" ? "col-5" : "col-6"}`}
+                              name={nameForm}
+                              rules={[...rules]}
+                              labelCol={{ span: 24 }}
+                            >
+                              <Input
+                                disabled={disabled}
+                                onBlur={handleFieldChange(nameForm)}
+                                suffix={`${name === FIELDS.percent ? "%" : ""}`}
+                                placeholder={placeholder}
+                              />
+                            </Form.Item>
+                            {or && <span>{or}</span>}
+                          </>
+                        );
+                    }
+                  }
+                )}
+            </>
+          );
+        });
+      }
+    });
+  };
+
+  return <>{renderTaxRules()}</>;
+}
+
+export default TaxFormItem;
---
+++ src/components/organisms/route/forms/tax-rules/tax.chemas.js
@@ -0,0 +1,226 @@
+import React from "react";
+import { createFormErrorComp } from "utils/form.util";
+import { REQUIRED_ERR, RegexConst } from "commons/consts";
+import { FormError } from "components/atoms";
+import numeral from "numeral";
+export const typeTAX = {
+  MAIN: "MAIN",
+  OTHER: "OTHER"
+};
+export const FIELDS = {
+  type: "type",
+  name: "name",
+  percent: "percent",
+  taxPayer: "taxPayer",
+  lumpSum: "lumpSum",
+  typeApply: "typeApply",
+  typeApplyOther: "typeApplyOther",
+  isLumSum: "isLumSum"
+};
+
+const LABELS = {
+  [FIELDS.type]: "Type of tax",
+  [FIELDS.name]: "Specify tax name",
+  [FIELDS.percent]: "Tax% applied to product ",
+  [FIELDS.taxPayer]: "To whom should these charges/ tax be applied ?",
+  [FIELDS.lumpSum]: "Lump-sum tax applied to the total net invoice (USD)",
+  [FIELDS.typeApply]: "Does this Trade Route and Product Category incur any Tax?",
+  [FIELDS.typeApplyOther]:
+    "Is there any other tax that is applicable to the total net invoice amount?"
+};
+
+export const taxRulesValue = [
+  { label: "VAT", value: "VAT" },
+  { label: "Sales Tax", value: "SALES" },
+  { label: "Other", value: "OTHER" }
+];
+
+export const taxTypeOtherValue = [
+  { label: "Custom and Duties", value: "CUSTOM_AND_DUTIES" },
+  { label: "Other", value: "OTHER" }
+];
+
+export const taxPayerValue = [
+  { label: "Buyer", value: "BUYER" },
+  { label: "Logistics", value: "LOGISTICS" },
+  { label: "Provider", value: "PROVIDER" }
+];
+
+export let hiddenFields = [FIELDS.taxName];
+
+export const TAX_RULES_TYPE_MAIN_SCHEMA = [
+  {
+    label: LABELS[FIELDS.typeApply],
+    name: FIELDS.typeApply,
+    type: "radio",
+    initValue: 0,
+    data: [
+      {
+        value: "MAIN",
+        name: "Yes"
+      },
+      {
+        value: 0,
+        name: "No"
+      }
+    ]
+  }
+];
+
+export const TAX_RULES_TYPE_OTHER_SCHEMA = [
+  {
+    label: LABELS[FIELDS.typeApplyOther],
+    name: FIELDS.typeApplyOther,
+    type: "radio",
+    initValue: 0,
+    data: [
+      {
+        value: "OTHER",
+        name: "Yes"
+      },
+      {
+        value: 0,
+        name: "No"
+      }
+    ]
+  }
+];
+
+export const TAX_RULES_MAIN_SCHEMA = [
+  {
+    label: LABELS[FIELDS.type],
+    name: FIELDS.type,
+    type: "select",
+    placeholder: LABELS[FIELDS.type],
+    initValue: [...taxRulesValue],
+    rules: [{ required: true, message: createFormErrorComp("Please choose the type of tax") }]
+  },
+  {
+    label: LABELS[FIELDS.name],
+    name: FIELDS.name,
+    type: "input",
+    placeholder: "Enter Tax Name",
+    initValue: null,
+    rules: [
+      {
+        required: true,
+        message: createFormErrorComp(REQUIRED_ERR("Please enter the tax name"))
+      }
+    ]
+  },
+  {
+    label: LABELS[FIELDS.percent],
+    name: FIELDS.percent,
+    type: "input",
+    initValue: null,
+    placeholder: "00.00",
+    rules: [
+      {
+        required: true,
+        message: createFormErrorComp(REQUIRED_ERR("the tax percentage"))
+      },
+      {
+        validator: (rule, value, callback) => {
+          const inputAmount = numeral(value).value();
+          if (inputAmount >= 0 && inputAmount < 100) {
+            return callback();
+          }
+          return callback(createFormErrorComp("The tax percentage max 2 number"));
+        }
+      }
+    ]
+  }
+];
+
+export const TAX_RULES_OTHER_SCHEMA = [
+  {
+    label: LABELS[FIELDS.taxPayer],
+    name: FIELDS.taxPayer,
+    type: "select",
+    placeholder: "Select Taxpayer",
+    initValue: [...taxPayerValue],
+    rules: [
+      {
+        required: true,
+        message: createFormErrorComp(REQUIRED_ERR("Please select "))
+      }
+    ]
+  },
+  {
+    label: LABELS[FIELDS.name],
+    name: FIELDS.name,
+    type: "input",
+    placeholder: "Enter Tax Name",
+    initValue: null,
+    rules: [
+      {
+        required: true,
+        message: createFormErrorComp(REQUIRED_ERR("Please enter the tax name"))
+      }
+    ]
+  },
+  {
+    label: LABELS[FIELDS.type],
+    name: FIELDS.type,
+    type: "select",
+    placeholder: LABELS[FIELDS.type],
+    initValue: [...taxTypeOtherValue],
+    rules: [{ required: true, message: createFormErrorComp("Please choose the type of tax") }]
+  },
+  {
+    label: LABELS[FIELDS.isLumSum],
+    name: FIELDS.isLumSum,
+    type: "radio",
+    initValue: 1,
+    classNames: "col-6",
+    data: [
+      {
+        value: 1,
+        name: "Tax% applied to total net invoice"
+      },
+      {
+        value: 0,
+        name: "Lump-sum tax applied to the total net invoice (USD)"
+      }
+    ]
+  },
+  {
+    label: null,
+    name: FIELDS.percent,
+    type: "input",
+    initValue: null,
+    placeholder: "00.00%",
+    disabled: false,
+    or: "OR",
+    rules: [
+      {
+        required: true,
+        message: createFormErrorComp(REQUIRED_ERR("the tax percentage"))
+      },
+      {
+        validator: (rule, value, callback) => {
+          const inputAmount = numeral(value).value();
+          if (inputAmount >= 0 && inputAmount < 100) {
+            return callback();
+          }
+          return callback(createFormErrorComp("The tax percentage max 2 number"));
+        }
+      }
+    ]
+  },
+  {
+    label: null,
+    name: FIELDS.lumpSum,
+    type: "input",
+    placeholder: "Lump-sum amount",
+    initValue: null,
+    disabled: true,
+    or: "",
+    rules: [
+      // {
+      //   required: true,
+      //   message: createFormErrorComp(REQUIRED_ERR("Please enter the Lump-sum amount"))
+      // }
+    ]
+  }
+];
