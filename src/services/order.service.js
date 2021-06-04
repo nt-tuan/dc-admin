11ab53d2b5cadbf97d6e7e240a0f1d7df2b21fe4
Nguyen Thanh Tuan <tuan.nguyen@morphotech.co.uk>
2021-06-04 11:07:05 +0700

    DS-446: fix order tables not working

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -2,13 +2,13 @@ import { backendAPI } from "utils/httpAPI.util";
 import { ApiPathConsts } from "commons/consts/system";
 
 export class OrderService {
-  static getAllActiveOrders = async () => {
-    const result = await backendAPI.get(ApiPathConsts.GET_ALL_ACTIVE_ORDERS);
+  static getAllActiveOrders = async (options) => {
+    const result = await backendAPI.get(ApiPathConsts.GET_ALL_ACTIVE_ORDERS, options);
     return result;
   };
 
-  static getAllOrderHistory = async () => {
-    const result = await backendAPI.get(ApiPathConsts.GET_ALL_ORDERS_HISTORY);
+  static getAllOrderHistory = async (options) => {
+    const result = await backendAPI.get(ApiPathConsts.GET_ALL_ORDERS_HISTORY, options);
     return result;
   };
 }
