ef09090e7ec29f83cf786a16d1b286dec8689cbc
Tuan Nguyen <tuan.nguyen@morphotech.co.uk>
2021-06-06 12:47:37 +0000

    Merged in DS-446 (pull request #90)
    DS-446
    
    * DS-446: fix order tables not working
    
    * DS-446: update api

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,14 +1,25 @@
 import { backendAPI } from "utils/httpAPI.util";
 import { ApiPathConsts } from "commons/consts/system";
-
+import dayjs from "dayjs";
+const DATETIME_FORMAT = "YYYY-MM-DDTHH:mm:ss";
 export class OrderService {
-  static getAllActiveOrders = async () => {
-    const result = await backendAPI.get(ApiPathConsts.GET_ALL_ACTIVE_ORDERS);
+  static getAllActiveOrders = async (options) => {
+    const { createdDate } = options;
+    const formattedCreatedDate = dayjs(createdDate).utc().format(DATETIME_FORMAT);
+    const result = await backendAPI.post(ApiPathConsts.GET_ALL_ACTIVE_ORDERS, undefined, {
+      ...options,
+      createdDate: formattedCreatedDate
+    });
     return result;
   };
 
-  static getAllOrderHistory = async () => {
-    const result = await backendAPI.get(ApiPathConsts.GET_ALL_ORDERS_HISTORY);
+  static getAllOrderHistory = async (options) => {
+    const { createdDate } = options;
+    const formattedCreatedDate = dayjs(createdDate).utc().format(DATETIME_FORMAT);
+    const result = await backendAPI.post(ApiPathConsts.GET_ALL_ORDERS_HISTORY, undefined, {
+      ...options,
+      createdDate: formattedCreatedDate
+    });
     return result;
   };
 }
