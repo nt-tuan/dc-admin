4620fe82215969d4f1d057e4fb67ab0642140ce8
Nguyen Thanh Tuan <thanhtuan12t@gmail.com>
2022-03-08 12:00:24 +0700

    chore: enable profiles (#146)
    Co-authored-by: Tuan Nguyen Thanh <tuan@distichain.com>

--- src/components/user-profile/constants/route-paths.const.js
+++ src/components/user-profile/constants/route-paths.const.js
@@ -1,6 +1,6 @@
 export const RoutePathEnum = {
-  PERSIONAL_INFORMATION: "/admin/profile/personal-information",
-  SECURITY: "/admin/profile/security",
-  TWO_FACTOR_AUTHENTICATION: "/admin/profile/two-factor-authentication",
-  NOTIFICATIONS: "/admin/profile/notifications"
+  PERSIONAL_INFORMATION: "/admin/v2/profile/personal-information",
+  SECURITY: "/admin/v2/profile/security",
+  TWO_FACTOR_AUTHENTICATION: "/admin/v2/profile/two-factor-authentication",
+  NOTIFICATIONS: "/admin/v2/profile/notifications"
 };
